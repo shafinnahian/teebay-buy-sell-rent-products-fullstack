061f2e6da50913b6b346c05b9016df6d786f21b3
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2024-03-30 00:01:40 +0600

    Fix

--- Client/src/views/OwnProductList/OwnProductList.jsx
+++ Client/src/views/OwnProductList/OwnProductList.jsx
@@ -16,25 +16,28 @@ const OwnProductList = () => {
         productList();
       }, [userID])
   return (
-        <div className='justify-center'>
-            <ul>
-                {data.map(product => (
-                    <li key={product.productId}>
-                        <h3>{product.name}</h3>
-                        <p>Description: {product.description}</p>
-                        <p>Price: {product.price}</p>
-                        <p>Rent Price: {product.rentPrice}</p>
-                        <p>Type: {product.type}</p>
-                        <p>Categories:</p>
-                        <ul>
-                            {product.categories.length > 0 && product.categories.map(category => (
-                                <li key={category.categoryid}>{category.name}</li>
-                            ))}
-                        </ul>
-                    </li>
-                ))}
-            </ul>
-        </div>
+    <div className="container mx-auto px-4 py-8">
+    <h1 className="text-3xl font-bold text-gray-800 mb-4">Your Products</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {data.map(product => (
+            <div key={product.productId} className="bg-white rounded-lg shadow-md p-6">
+                <h3 className="text-xl font-semibold text-gray-800 mb-2">{product.name}</h3>
+                <p className="text-gray-600 mb-2">Description: {product.description}</p>
+                <p className="text-gray-600 mb-2">Price: {product.price}</p>
+                <p className="text-gray-600 mb-2">Rent Price: {product.rentprice}</p>
+                <p className="text-gray-600 mb-2">Type: {product.type === true? "Sell":"Rent"}</p>
+                <div className="text-gray-600 mb-2">
+                    <p>Categories:</p>
+                    <ul className="list-disc list-inside">
+                        {product.Category && product.Category.length > 0 && product.Category.map(Category => (
+                            <li key={Category.categoryid}>{Category}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        ))}
+    </div>
+</div>
   )
 }
 
